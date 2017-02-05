f795a0185bbc24b7c1ce775d7857a6168dba50a7
Jacob Solomon <j.solomon@knights.ucf.edu>
2017-02-04 20:55:49 -0500

    Initial Shortest Job First

--- Project1/sjf.c
+++ Project1/sjf.c
@@ -1,5 +1,36 @@
 #include "sjf.h"
-void sjf(){
-    
-    printf("TEST OF sjf\n"); //Test for linking files
+#include "main.h"
+
+struct arrivedProcesses {
+  char  name[4];
+  int   index;
+  int   burstTime;
+
+};
+
+struct arrivedProcesses* arrivedProcesses;
+
+void sjf(struct testcase sjf){
+  int time;
+  int tempProcCount = 0;
+
+  int i, p[sjf.numOfProcesses];//q is an order of arrived processes
+
+  for(time = 0; time < sjf.runTime; time++)//run through the total run time
+  {
+    for(i = 0; i < sjf.numOfProcesses;i++)//run through the number of processes
+    {
+      if(sjf.processes[i].arrival == time)
+      {
+        printf("Process %s has arrived!", sjf.processes[i].name);
+        sjf.processes[i].arrived = 1;
+
+        strcpy(arrivedProcesses[tempProcCount].name, sjf.processes[i].name);
+        arrivedProcesses[tempProcCount].burstTime = sjf.processes[i].burst;
+
+
+      }
+    }
+  }
+  printf("TEST OF sjf\n"); //Test for linking files
 }
--- Project1/sjf.h
+++ Project1/sjf.h
@@ -2,8 +2,8 @@
 #define sjf_h
 
 #include <stdio.h>
-
+struct testcase;
 //Function Prototypes
-void sjf();
+void sjf(struct testcase);
 
 #endif
