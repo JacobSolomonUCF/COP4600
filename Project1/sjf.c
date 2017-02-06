62d61783924066457198cc0386e3d2d261516595
Jacob Solomon <j.solomon@knights.ucf.edu>
2017-02-06 14:16:29 -0500

    Update to print to file

--- Project1/sjf.c
+++ Project1/sjf.c
@@ -4,12 +4,15 @@
 int lowest = -1;
 int findL(struct testcase sjf);
 void sjf(struct testcase sjf){
+
+  FILE * out = fopen("processes.out","w");
+
   int curpos = -1;
   int time;
   int inuse = 0;
   int i,j;
 
-  printf("%d processes\nUsing Shortest Job First (Pre)\n\n",sjf.numOfProcesses );
+  fprintf(out,"%d processes\nUsing Shortest Job First (Pre)\n\n",sjf.numOfProcesses );
 
   for(time = 0; time < sjf.runTime; time++)//run through the total run time
   {
@@ -17,14 +20,14 @@ void sjf(struct testcase sjf){
     {
       if(sjf.processes[i].arrival == time)
       {
-        printf("Time %d: %s arrived\n",time, sjf.processes[i].name);
+        fprintf(out,"Time %d: %s arrived\n",time, sjf.processes[i].name);
         sjf.processes[i].arrived = 1;
         sjf.processes[i].turnaround = sjf.processes[i].burst;
 
       }
     }
     if(sjf.processes[curpos].burst == 0 && curpos != -1){
-      printf("Time %d: %s finished\n",time, sjf.processes[curpos].name);
+      fprintf(out,"Time %d: %s finished\n",time, sjf.processes[curpos].name);
       sjf.processes[curpos].arrived = -1;
       sjf.processes[curpos].finished = time;
       lowest = -1;
@@ -35,7 +38,7 @@ void sjf(struct testcase sjf){
       next = findL(sjf);
       if(next != curpos){
         curpos = next;
-        printf("Time %d: %s selected (burst %d)\n", time, sjf.processes[curpos].name,sjf.processes[curpos].burst);
+        fprintf(out,"Time %d: %s selected (burst %d)\n", time, sjf.processes[curpos].name,sjf.processes[curpos].burst);
       }
 
     }
@@ -49,10 +52,10 @@ void sjf(struct testcase sjf){
       }
       if(flag == 1){
         curpos = findL(sjf);
-        printf("Time %d: %s selected (burst %d)\n", time, sjf.processes[curpos].name,sjf.processes[curpos].burst);
+        fprintf(out,"Time %d: %s selected (burst %d)\n", time, sjf.processes[curpos].name,sjf.processes[curpos].burst);
         inuse = 1;
       }else{
-        printf("Time %d: IDLE\n",time );
+        fprintf(out,"Time %d: IDLE\n",time );
       }
 
     }
@@ -60,13 +63,14 @@ void sjf(struct testcase sjf){
 
     sjf.processes[curpos].burst--;
   }
-  printf("Finished at time %d\n\n", time);
+  fprintf(out,"Finished at time %d\n\n", time);
   for(i = 0; i<sjf.numOfProcesses; i++){
     int TA = sjf.processes[i].finished - sjf.processes[i].arrival;
     int Wait = TA - sjf.processes[i].turnaround;
-    printf("%s wait %d turnaround %d \n",sjf.processes[i].name,Wait,TA );
+    fprintf(out,"%s wait %d turnaround %d \n",sjf.processes[i].name,Wait,TA );
   }
 
+  fclose(out);
 }
 int findL(struct testcase sjf){
   int i;
