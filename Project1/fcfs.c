5ad98a156eb0685603ba5470a075543917754eda
Jacob Solomon <j.solomon@knights.ucf.edu>
2017-02-06 14:16:21 -0500

    Update to print to file

--- Project1/fcfs.c
+++ Project1/fcfs.c
@@ -2,18 +2,19 @@
 #include "main.h"
 
 void fcfs(struct testcase fcfs){
+  FILE * out = fopen("processes.out","w");
   int flag = 0,position = 0, index = 0 ;
   int time = 0, i = 0, inuse = 0;
   int q[fcfs.numOfProcesses];
 
   //Header
-  printf("%d processes\nUsing First Come First Served\n\n",fcfs.numOfProcesses);
+  fprintf(out, "%d processes\nUsing First Come First Served\n\n",fcfs.numOfProcesses);
 
   //Core Loop
   for(time = 0; time <= fcfs.runTime;time++){
     for(i = 0; i<fcfs.numOfProcesses; i++){ //Checking for arrival
       if(fcfs.processes[i].arrival == time){
-        printf("Time %d: %s arrived\n", time,fcfs.processes[i].name );
+        fprintf(out,"Time %d: %s arrived\n", time,fcfs.processes[i].name );
         fcfs.processes[i].arrived = time;
         q[index] = i;
         index++;
@@ -23,7 +24,7 @@ void fcfs(struct testcase fcfs){
       int firstTime = 0;
       if(fcfs.processes[q[position]].burst == 0){
 
-        printf("Time %d: %s finished\n",time,fcfs.processes[q[position]].name);
+        fprintf(out,"Time %d: %s finished\n",time,fcfs.processes[q[position]].name);
         fcfs.processes[q[position]].finished = (time- fcfs.processes[q[position]].arrival);
         flag++;
         position++;
@@ -33,21 +34,21 @@ void fcfs(struct testcase fcfs){
           firstTime = 1;
           while(time < fcfs.runTime){
             if(firstTime != 1){
-              printf("Time %d: IDLE\n",time );
+              fprintf(out,"Time %d: IDLE\n",time );
               time++;
             }else{
               time++;
               firstTime = 0;
             }
           }
-          printf("Finished at time %d\n\n", time);
+          fprintf(out,"Finished at time %d\n\n", time);
           break;
         }
       }
     }
 
     if(inuse == 0){ //If the processor is free
-        printf("Time %d: %s selected (burst %d)\n",time,fcfs.processes[q[position]].name, fcfs.processes[q[position]].burst);
+        fprintf(out,"Time %d: %s selected (burst %d)\n",time,fcfs.processes[q[position]].name, fcfs.processes[q[position]].burst);
         fcfs.processes[q[position]].wait = time - fcfs.processes[q[position]].arrived;
         inuse = 1;
     }
@@ -57,10 +58,10 @@ void fcfs(struct testcase fcfs){
 
   //Printing wait & Turn around time
   for(i = 0; i<fcfs.numOfProcesses; i++){
-      printf("%s wait %d turnaround %d\n",fcfs.processes[i].name, fcfs.processes[i].wait, fcfs.processes[i].finished );
+      fprintf(out,"%s wait %d turnaround %d\n",fcfs.processes[i].name, fcfs.processes[i].wait, fcfs.processes[i].finished );
     }
 
 
 
-
+fclose(out);
 }
